dcff41bac9c967c235a2c242d10ee3cc2f719f15
aleksandrgineyka <meinepostkam@gmail.com>
2023-06-08 14:39:35 +0300

    feat: добавить service, storage и controller для сущности Director

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/DirectorStorage.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Director;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DirectorStorage {
+    List<Director> getAll();
+
+    Director create(Director director);
+
+    Optional<Director> update(Director director);
+
+    boolean delete(Integer id);
+
+    Optional<Director> getById(Integer id);
+}
