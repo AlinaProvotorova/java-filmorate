8d730fbba0e0ed844283cc529d994729eb975a6e
kaldubasina <kaldubasina@yandex.ru>
2023-06-15 14:39:41 +0300

    style: fix style

--- src/test/java/ru/yandex/practicum/filmorate/storage/dao/FeedDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/dao/FeedDbStorageTest.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage.dao;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
